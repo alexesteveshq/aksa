43e7632bcb85abebfb6c48ca12ea995fb4d54f84
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-10-25 01:18:40 +0200

    [Alex] desglosar iva para costo de mercancia

---
+++ account_aksa/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Aksa',
+    'summary': 'Account module extension for Aksa',
+    'description': 'Account module extension for Aksa',
+    'category': 'Account',
+    'author': 'Alex Esteves',
+    'depends': [
+        'stock_account',
+    ],
+    'data': [
+        'views/product_category_views.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
